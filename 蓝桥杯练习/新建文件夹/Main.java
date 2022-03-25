26a0434a00d4328db0241a72b92705b102bd79ab
Lhy1122 <1319628965@qq.com>
2022-03-25 21:37:02 +0800

    exercise

---
+++ 蓝桥杯练习/新建文件夹/Main.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Main{
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        double c = sc.nextDouble();
+
+        sc.close();
+
+        double res = 0;
+
+        double max = 0;
+        if(a > max)
+            max = a;
+        if(b > max)
+            max = b;
+        if(c > max)
+            max = c;
+
+        for(double i = 1; i <= max; i++){
+            if(a % i == 0 && b % i == 0){
+                res++;
+                continue;
+            }
+            if(a % i == 0 && c % i == 0){
+                res++;
+                continue;
+            }
+            if(b % i == 0 && c % i == 0){
+                res++;
+                continue;
+            }
+        }
+
+        int ress = (int)res;
+
+        System.out.println(ress);
+    }
+}
