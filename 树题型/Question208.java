5b249aa5d2eef1d895c220677ab2af50502705e2
Lhy1122 <1319628965@qq.com>
2022-03-29 00:09:43 +0800

    exercise

---
+++ 树题型/Question208.java
@@ -0,0 +1,66 @@
+import java.util.TreeMap;
+
+class Trie {
+
+    private class Node{
+
+        public boolean isWord;
+        public TreeMap<Character, Node> next;
+
+        public Node(boolean isWord){
+            this.isWord = isWord;
+            next = new TreeMap<>();
+        }
+
+        public Node(){
+            this(false);
+        }
+    }
+
+    private Node root;
+
+    public Trie() {
+            root = new Node();
+    }
+    
+    public void insert(String word) {
+        Node cur = root;
+        for(int i = 0; i < word.length(); i++){
+            char c = word.charAt(i);
+            if(cur.next.get(c) == null)
+                cur.next.put(c, new Node());
+            cur = cur.next.get(c);
+        }
+        cur.isWord = true;
+    }
+    
+    public boolean search(String word) {
+        Node cur = root;
+        for(int i = 0; i < word.length(); i++){
+            char c = word.charAt(i);
+            if(cur.next.get(c) == null)
+                return false;
+            cur = cur.next.get(c);
+        }
+        return cur.isWord;
+    }
+    
+    public boolean startsWith(String prefix) {
+        Node cur = root;
+        for(int i = 0; i < prefix.length(); i++){
+            char c = prefix.charAt(i);
+            if(cur.next.get(c) == null)
+                return false;
+            cur = cur.next.get(c);
+        }
+        return true;
+    }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
